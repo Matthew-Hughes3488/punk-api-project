5e48bc184336fd1d4c6202c93367d842aa276bc3
Matthew <12hughesm@gmail.com>
2023-12-21 15:35:01 +0000

    Finished main component outline

---
+++ src/containers/Main/Main.tsx
@@ -0,0 +1,14 @@
+import { Beer } from "../../types/types"
+import "./Main.scss"
+
+type mainProps = {
+    beers: Beer[]
+}
+
+export const main = ({beers} : mainProps) => {
+  return (
+    <main>
+
+    </main>
+  )
+}
