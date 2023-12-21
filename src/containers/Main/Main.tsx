1295048d387271e338243762f8db93fd2764bf41
Matthew <12hughesm@gmail.com>
2023-12-21 15:46:01 +0000

    Updated main component

--- src/containers/Main/Main.tsx
+++ src/containers/Main/Main.tsx
@@ -1,4 +1,5 @@
 import { Beer } from "../../types/types"
+import { CardList } from "../CardList/CardList"
 import "./Main.scss"
 
 type mainProps = {
@@ -8,7 +9,7 @@ type mainProps = {
 export const main = ({beers} : mainProps) => {
   return (
     <main>
-
+      <CardList beers={beers}/>
     </main>
   )
 }
