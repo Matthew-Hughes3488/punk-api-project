8c1f44dea154de9d6d90fa91769876e557de69bd
Matthew <12hughesm@gmail.com>
2023-12-21 17:52:57 +0000

    Website now displays data from an API fetch instead of from the data folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Main } from "./components/Main/Main";
-import beers from "./data/beers";
+import { Beer } from "./types/types";
 import "./App.scss";
 import { NavBar } from "./components/NavBar/NavBar";
 
 function App() {
+  const [beers, setBeers] = useState<Beer[]>([]);
+
+  useEffect(() => {
+    const getBeerData = async () => {
+      const data = await fetch("https://api.punkapi.com/v2/beers?per_page=6");
+      const beersData: Beer[] = await data.json();
+      setBeers(beersData);
+    };
+
+    getBeerData();
+  }, []);
 
   return (
     <>
-      <section className="website-body"> {/* class name is a placeholder, update it */}
+      <section className="website-body">
+        {" "}
+        {/* class name is a placeholder, update it */}
         <NavBar />
         <Main beers={beers} />
       </section>
