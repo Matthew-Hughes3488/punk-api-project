d7093ca8ff231382c87b8324c58d896f2a3a0ffe
Matthew <12hughesm@gmail.com>
2023-12-27 23:20:24 +0000

    Finished styling the navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,19 +1,29 @@
-import { SearchBox } from '../SearchBox/SearchBox'
-import { FilterList } from '../FilterList/FilterList'
-import { ChangeEventHandler } from 'react';
+import { SearchBox } from "../SearchBox/SearchBox";
+import { FilterList } from "../FilterList/FilterList";
+import { ChangeEventHandler } from "react";
+import "./NavBar.scss"
 
 type navBarProps = {
   onSearchBoxChange: ChangeEventHandler<HTMLInputElement>;
   onABVChange: ChangeEventHandler<HTMLInputElement>;
   onClassicRangeChange: ChangeEventHandler<HTMLInputElement>;
   onAcidityChange: ChangeEventHandler<HTMLInputElement>;
-}
+};
 
-export const NavBar = ({onSearchBoxChange, onABVChange, onAcidityChange, onClassicRangeChange} : navBarProps) => {
+export const NavBar = ({
+  onSearchBoxChange,
+  onABVChange,
+  onAcidityChange,
+  onClassicRangeChange,
+}: navBarProps) => {
   return (
-    <section>
-        <SearchBox onSearchBoxChange={onSearchBoxChange}/>
-        <FilterList onABVChange={onABVChange} onAcidityChange={onAcidityChange} onClassicRangeChange={onClassicRangeChange}/>
+    <section className="nav-bar">
+      <SearchBox onSearchBoxChange={onSearchBoxChange} />
+      <FilterList
+        onABVChange={onABVChange}
+        onAcidityChange={onAcidityChange}
+        onClassicRangeChange={onClassicRangeChange}
+      />
     </section>
-  )
-}
+  );
+};
