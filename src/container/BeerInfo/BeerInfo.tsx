d0517cf7af8d2ccba270889938151da61a6d7ce8
Matthew <12hughesm@gmail.com>
2023-12-30 14:01:40 +0000

    Added styles to beer info page

--- src/container/BeerInfo/BeerInfo.tsx
+++ src/container/BeerInfo/BeerInfo.tsx
@@ -2,7 +2,7 @@ import { BeerIngredientList } from "../../components/BeerIngredientList/BeerIngr
 import { FoodPairingList } from "../../components/FoodPairingList/FoodPairingList";
 import { Beer } from "../../types/types";
 import { Link, useParams } from "react-router-dom";
-import "./BeerInfo.scss"
+import "./BeerInfo.scss";
 import { BeerDescription } from "../../components/BeerDescription/BeerDescription";
 
 type BeerInfoProps = {
@@ -21,10 +21,19 @@ export const BeerInfo = ({ beers }: BeerInfoProps) => {
       <Link to="/">
         <h1 className="beer-info__return-link">Return</h1>
       </Link>
-      <h1 className="beer-info-page__name">{`${beer.name} - ${beer.tagline}`}</h1>
-      <BeerDescription beer={beer}/>
-      <BeerIngredientList beer={beer}/>
-      <FoodPairingList beer={beer}/>
+      <section>
+        <h1 className="beer-info-page__name">{`${beer.name} - ${beer.tagline}`}</h1>
+        <BeerDescription beer={beer} />
+        <BeerIngredientList beer={beer} />
+        <FoodPairingList beer={beer} />
+      </section>
+      <section className="beer-info-page__image-container">
+        <img
+          className="beer-info-page__image-container--image"
+          src={beer.image_url}
+          alt="image of the beer"
+        />
+      </section>
     </section>
   );
 };
