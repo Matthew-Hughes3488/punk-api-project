04144623efc9992a8028f31cee2976e1a1f2ade4
Matthew <12hughesm@gmail.com>
2023-12-30 15:12:28 +0000

    Made a new BeerInfoCard container

--- src/container/BeerInfo/BeerInfo.tsx
+++ src/container/BeerInfo/BeerInfo.tsx
@@ -1,10 +1,7 @@
-import { BeerIngredientList } from "../../components/BeerIngredientList/BeerIngredientList";
-import { FoodPairingList } from "../../components/FoodPairingList/FoodPairingList";
 import { Beer } from "../../types/types";
 import { Link, useParams } from "react-router-dom";
 import "./BeerInfo.scss";
-import { BeerDescription } from "../../components/BeerDescription/BeerDescription";
-import { useEffect, useRef } from "react";
+import { BeerInfoCard } from "../BeerInfoCard/BeerInfoCard";
 
 type BeerInfoProps = {
   beers: Beer[];
@@ -17,37 +14,9 @@ export const BeerInfo = ({ beers }: BeerInfoProps) => {
   const beer = beers.find((beer) => beer.id == beerId);
   if (!beer) return <p>Couldn't find the beers page</p>;
 
-  const containerRef = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    const container = containerRef.current;
-    if (container) {
-      if (container.scrollHeight > container.clientHeight) {
-        container.style.height = container.scrollHeight + 10 + "px";
-      }
-    }
-  }, [beer]);
-
   return (
     <section className="beer-info-container">
-      <section ref={containerRef} className="beer-info-card">
-        <Link to="/">
-          <h1 className="beer-card-link">Return</h1>
-        </Link>
-        <section>
-          <h1 className="beer-info-card__name">{`${beer.name} - ${beer.tagline}`}</h1>
-          <BeerDescription beer={beer} />
-          <BeerIngredientList beer={beer} />
-          <FoodPairingList beer={beer} />
-        </section>
-        <section className="beer-info-card__image-container">
-          <img
-            className="beer-info-card__image-container--image"
-            src={beer.image_url}
-            alt="image of the beer"
-          />
-        </section>
-      </section>
+      <BeerInfoCard beer={beer}/>
     </section>
   );
 };
